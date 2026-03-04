a5c7956c043ed3b779e79469291aa03936f8b653
Ajsi Sala <AjsSal20@htl-shkoder.com>
2026-03-04 12:41:16 +0100

    besser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,14 @@
 import background from '$lib/assets/bg.jpg';
 </script>
 
+<svelte:head>
+    <title>Jewelry - Webshop</title>
+    <meta name="description" content="Discover our exquisite collection of jewelry, crafted with precision and passion. From elegant necklaces to stunning rings, find the perfect piece to complement your style. Shop now and elevate your look with our timeless designs." />
+</svelte:head>
+
 <header class="h-[500px] bg-cover bg-center flex items-center justify-center"style="background-image: url('{background}')">
 
-    <h1 class="text-5xl">Electronic Devices</h1>
+    <h1 class="text-5xl">Jewelry </h1>
 
 </header>
 

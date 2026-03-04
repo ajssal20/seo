17b8be566435321e3e1d56088d787b52e3a24e01
Ajsi Sala <AjsSal20@htl-shkoder.com>
2026-03-04 12:28:32 +0100

    das ist ein Test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+import background from '$lib/assets/bg.jpg';
+</script>
+
+<header class="h-[500px] bg-cover bg-center flex items-center justify-center"style="background-image: url('{background}')">
+
+    <h1 class="text-5xl">Electronic Devices</h1>
+
+</header>
+
+<main>
+
+
+</main>
+
+
+<footer>
+
+</footer>
